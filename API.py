95972d4d0be4d42fbdc718349ac1f4bbf94a150d
ProGerald1113 <brown.gerald.2008@gmail.com>
2023-10-04 16:46:48 +0100

    Functions - Weather info and IP

--- API.py
+++ API.py
@@ -2,25 +2,48 @@ import requests
 
 
 #finds more or less where user resides 
-location_api = 'https://ipapi.co/json/'
-response = requests.get(location_api)
 
-if response.status_code == 200:
-    longitude = print(response.json()["longitude"])
-    latitude = print(response.json()["latitude"])
+def get_location():
+    location_api = 'https://ipapi.co/json/'
+    response = requests.get(location_api)
 
-    coordinates = {"longitude":longitude , "latitude" : latitude}
+    if response.status_code == 200:
+        longitude = response.json()["longitude"]
+        latitude =  response.json()["latitude"]
 
-else:
-    print(f"error{response.status_code}")
+        coordinates = {"longitude" : longitude , "latitude" : latitude}
 
-weather_api = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
-weather_response = requests.get(weather_api)
+    else:
+        print(f"error{response.status_code}")
+    
+    return coordinates
 
-if weather_response.status_code == 200:
-    print(weather_response.json())
+def weather_info():
+    location = get_location() 
 
-else:
-    print(f"error:{weather_response.status_code}")
+    longitude = location["longitude"]
+
+    latitude = location["latitude"]
+
+    weather_api = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
+
+    weather_response = requests.get(weather_api)
+
+    if weather_response.status_code == 200:
+        print(weather_response.json())
+ 
+    else:
+        print(f"error: {weather_response.status_code}")
+    
+    return weather_info
+
+print(get_location())
+
+#print(weather_info())
+
+
+
+
+
 
 
